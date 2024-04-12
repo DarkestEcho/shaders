51f5c069c38297e99dfc0af4b7c4f6cb0908dc26
Dmitriy <samarin99@gmail.com>
2024-04-12 12:19:17 +0300

    Added 002 shader

--- main.cpp
+++ main.cpp
@@ -36,14 +36,25 @@ int main(int, char**)
 
     glViewport(0, 0, 800, 600);
 
+    // telling GLFW we want to call this function on every window resize
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    Shader shader("../shaders/shader_001.vs", "../shaders/shader_001.fs");
+    // build and compile our shader program
+ // ------------------------------------
+ // vertex shader
+    Shader shader("../shaders/shader_002.vs", "../shaders/shader_002.fs");
 
     float vertices[] = {
         0.0f,  0.5f, 0.0f,  // top right
         0.5f, -0.5f, 0.0f,  // bottom right
         -0.5f, -0.5f, 0.0f,  // bottom left
+    };
+
+    float vertices_colors[] = {
+        // positions         // colors
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
     };
 
     unsigned int VBO, VAO;
@@ -54,10 +65,13 @@ int main(int, char**)
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_colors), vertices_colors, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     glBindVertexArray(0);
 
@@ -72,13 +86,14 @@ int main(int, char**)
         glClear(GL_COLOR_BUFFER_BIT);
 
         shader.Use();
-
-        float timeValue = glfwGetTime();
-        float greenValue = sin(timeValue) / 2.0f + 0.5f;
-        shader.SetValue("ourColor", { 0.0f, greenValue, 0.0f, 1.0f });
-
+        /*
+                float timeValue = glfwGetTime();
+                float greenValue = sin(timeValue) / 2.0f + 0.5f;
+                shader.SetValue("ourColor", { 0.0f, greenValue, 0.0f, 1.0f });
+        */
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glBindVertexArray(0); // no need to unbind it every time 
 
         // check and call events and swap the buffers
         glfwPollEvents();
